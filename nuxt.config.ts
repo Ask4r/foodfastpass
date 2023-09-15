5b58c1e9be08bddf6ca39da0efaf65fac93b7cb9
Askar <dv4askar@gmail.com>
2023-09-16 02:26:55 +0300

    initial

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -43,6 +43,8 @@ export default defineNuxtConfig({
   ],
   nitro: {
     prerender: {
+      crawlLinks: false,
+      failOnError: false,
       routes: [ '/', '/suggest', '/restaurants' ],
     },
     devHandlers: [],
